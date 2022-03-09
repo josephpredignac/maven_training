2433a9e7a4f76ddd40f9dbbeac3966b47b11634a
joseph.predignac <joseph.predignac@epita.fr>
2022-03-09 16:29:22 +0100

    Add test to match 100% coverage

---
+++ src/test/java/fr/lernejo/SampleTest.java
@@ -0,0 +1,49 @@
+package fr.lernejo;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SampleTest {
+    @Test
+    void op_add_basic() {
+        Sample sample = new Sample();
+        Assertions.assertEquals(sample.op(Sample.Operation.ADD, 10, 10), 20);
+    }
+    @Test
+    void op_add_neg() {
+        Sample sample = new Sample();
+        Assertions.assertEquals(sample.op(Sample.Operation.ADD, 10, -10), 0);
+    }
+    @Test
+    void op_mult_basic() {
+        Sample sample = new Sample();
+        Assertions.assertEquals(sample.op(Sample.Operation.MULT, 10, 10), 100);
+    }
+    @Test
+    void op_mult_neg() {
+        Sample sample = new Sample();
+        Assertions.assertEquals(sample.op(Sample.Operation.MULT, 10, -10), -100);
+    }
+    @Test
+    void op_mult_zero() {
+        Sample sample = new Sample();
+        Assertions.assertEquals(sample.op(Sample.Operation.MULT, 10, 0), 0);
+    }
+    @Test
+    void fact_basic() {
+        Sample sample = new Sample();
+        Assertions.assertEquals(sample.fact(5), 120);
+    }
+    @Test
+    void fact_one() {
+        Sample sample = new Sample();
+        Assertions.assertEquals(sample.fact(1), 1);
+    }
+    @Test
+    void fact_zero() {
+        Sample sample = new Sample();
+        Assertions.assertEquals(sample.fact(0), 1);
+    }
+}
